13f9ca5c8351f022c7e6d7157ac782e1fbc7f72a
Genu <Genu@Genu-HP>
2016-06-06 18:25:03 -0300

    criação do migrate do BD

---
+++ database/migrations/2016_06_06_210425_create_funcionarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome',120);
+            $table->string('email',80);
+            $table->tinyInteger('id_secao');
+            $table->string('fone',9);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('funcionarios');
+    }
+}
