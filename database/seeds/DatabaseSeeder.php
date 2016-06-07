4466386cf17c9a37b71bdace34da53493cadbd8d
Genu <Genu@Genu-HP>
2016-06-07 16:03:57 -0300

    criação do seeder para inserção de dados para teste na tabela funcionarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,25 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+    public function run() {
+         $this->call('FuncionarioTableSeeder');
+        }
 }
+
+class FuncionarioTableSeeder extends Seeder{
+        public function run() {
+            DB::insert('insert into funcionarios(nome, email, id_secao, fone)
+            values (?,?,?,?)', array('JOÃO DA SILVA', 'jsilva@email.com', '1', '980706050'));
+            DB::insert('insert into funcionarios(nome, email, id_secao, fone)
+            values (?,?,?,?)', array('JOSÉ MANOEL ', 'zemane@email.com', '3', '950607080'));
+    
+        }
+
+    }
