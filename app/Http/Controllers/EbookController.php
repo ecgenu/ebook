a70cffbc91ef99f4502f4a56b3241ddfcbccda68
Genu <Genu@Genu-HP>
2016-06-07 16:56:04 -0300

    atualização do controller para chamar a view após a consulta

--- app/Http/Controllers/EbookController.php
+++ app/Http/Controllers/EbookController.php
@@ -6,15 +6,9 @@ use Illuminate\Support\Facades\DB;
 class EbookController extends Controller
 {
     public function lista() {
-        $html ='<h1>Dados do Ebook</h1>';
         $funcionarios = DB::select('select * from funcionarios');
-        foreach ($funcionarios as $f){
-               $html .='Nome: ' . $f->nome.'<br>
-                       email: ' . $f->email.'<br>
-                       seção: ' . $f->id_secao.'<br>
-                       Telefone: ' . $f->fone. '<br><br>';
-        }
-        return $html;
+        
+        return view('lista')->with('funcionarios',$funcionarios);
         
     }
 }
