2d86138be5068802d6c999437b7ee5b0e7749b62
Genu <Genu@Genu-HP>
2016-06-07 16:40:18 -0300

    criação do método lista no EbookController, para mostrar os dados da tb funcionarios

--- app/Http/Controllers/EbookController.php
+++ app/Http/Controllers/EbookController.php
@@ -1,14 +1,20 @@
 <?php
 
 namespace genu\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-use genu\Http\Requests;
+use Illuminate\Support\Facades\DB;
 
 class EbookController extends Controller
 {
     public function lista() {
+        $html ='<h1>Dados do Ebook</h1>';
+        $funcionarios = DB::select('select * from funcionarios');
+        foreach ($funcionarios as $f){
+               $html .='Nome: ' . $f->nome.'<br>
+                       email: ' . $f->email.'<br>
+                       seção: ' . $f->id_secao.'<br>
+                       Telefone: ' . $f->fone. '<br><br>';
+        }
+        return $html;
         
-    }//
+    }
 }
